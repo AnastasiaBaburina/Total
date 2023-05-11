1d8b9c9a1416f6ad107736c49972042a91593054
Anastasia Baburina <nastia_babur@mail.ru>
2023-05-12 01:20:12 +0300

    Описаны методы ввода и вывода массива

--- Program.cs
+++ Program.cs
@@ -1,6 +1,23 @@
-﻿//тут
-void showArray (int[] array)
+﻿
+string[] CreateStringArray (int size)
 {
-    for (int i = 0; i<array.Length; i++) Console.Write(array[i] + " ");
+    string[] array = new string[size];
+    Console.WriteLine("Введите " + size + " элементов массива");
+    for (int i = 0; i < size; i++)
+        {
+            array[i] = Convert.ToString(Console.ReadLine());
+        }   
+    return array;
+}
+
+void showArray (string[] array)
+{
+    for (int i = 0; i<array.Length - 1; i++) Console.Write(array[i] + " , ");
+    Console.Write(array[array.Length - 1]);
     Console.WriteLine();
 }
+
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] myArray = CreateStringArray(size);
+showArray(myArray);
