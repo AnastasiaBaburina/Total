8ab675c7cdc36e3e1529ac9d6d03738c8d4a022a
Anastasia Baburina <nastia_babur@mail.ru>
2023-05-13 00:47:38 +0300

    Описан метод

--- Program.cs
+++ Program.cs
@@ -17,7 +17,26 @@ void showArray (string[] array)
     Console.WriteLine();
 }
 
+string[] ShortStringArray (string[] array)
+{
+    int size = 0;
+    for (int i = 0; i < array.Length; i++)
+        if ( array[i].Length < 4) size+=1;
+    int index = 0;
+    string[] newArray = new string[size]; 
+    for (int i = 0; i < array.Length; i++)
+        if ( array[i].Length < 4)
+        {
+            newArray[index] = array[i];
+            index+=1;
+        }
+    return newArray;
+}
+
 Console.Write("Введите размер массива: ");
 int size = Convert.ToInt32(Console.ReadLine());
 string[] myArray = CreateStringArray(size);
 showArray(myArray);
+string[] newArray = ShortStringArray (myArray);
+Console.WriteLine("Строки, длина которых <= 3 : ");
+showArray(newArray);
